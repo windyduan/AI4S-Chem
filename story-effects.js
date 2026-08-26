eec0d489edcf8a1688531855989221aa1bbbf79e
windyduan <136797961+windyduan@users.noreply.github.com>
2026-08-26 09:41:16 +0800

    bump Course 01 case alignment cache

--- story-effects.js
+++ story-effects.js
@@ -36,5 +36,5 @@
                                                         load('p26-p27-review.js?v=20260825a',()=>
                                                           load('p28-review.js?v=20260825a',()=>
                                                             load('p29-p39-review.js?v=20260825a',()=>
-                                                              load('final-alignment.js?v=20260825c')))))))))))))))))))))))))))))));
+                                                              load('final-alignment.js?v=20260826a')))))))))))))))))))))))))))))));
 })();
