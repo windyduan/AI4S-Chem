9c42bed83522d0de0c4746f68170edd145fc6362
windyduan <136797961+windyduan@users.noreply.github.com>
2026-08-26 10:20:40 +0800

    加载案例问题导向重构

--- story-effects.js
+++ story-effects.js
@@ -37,5 +37,6 @@
                                                           load('p28-review.js?v=20260825a',()=>
                                                             load('p29-p39-review.js?v=20260825a',()=>
                                                               load('final-alignment.js?v=20260826a',()=>
-                                                                load('case-layout-main-fix.js?v=20260826b'))))))))))))))))))))))))))))))));
+                                                                load('case-layout-main-fix.js?v=20260826b',()=>
+                                                                  load('case-story-main.js?v=20260826a')))))))))))))))))))))))))))))))));
 })();
